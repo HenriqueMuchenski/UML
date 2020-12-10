3fe2f8fe1ffec314eaf8c99579a47182a2b5fe79
HenriqueMuchenski <hjmuchenski@gmail.com>
2020-12-10 04:59:03 -0300

    Testando a classe de domínio Categoria

--- 06_Spring_UML_implementation/src/main/java/com/umlspring/demo/resources/CategoriaResource.java
+++ 06_Spring_UML_implementation/src/main/java/com/umlspring/demo/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.umlspring.demo.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.umlspring.demo.domain.Categoria;
 
 // Definindo esta classe como uma classe de controle
 @RestController
@@ -11,7 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "teste categorias rest";
+	public List<Categoria> listar() {
+		
+		List<Categoria> categorias = Arrays.asList(
+				new Categoria(1 ,"teste1"), 
+				new Categoria(2, "teste2"));
+		
+		return categorias;
 	}
 }
